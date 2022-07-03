a92adfc52723b8b4d9d4fda1ba2478b664127b93
ohasi <ohadsirkis@gmail.com>
2022-07-03 18:06:31 +0300

    Added handlers for pages

---
+++ View/gameView.js
@@ -0,0 +1,16 @@
+class GameView{
+    constructor()
+    {
+        this.view = document.getElementById('game');
+        this.userInfo = document.getElementById('user-info');
+        this.changeUser = document.getElementById('change-user');
+    }
+
+    generateView(board){
+        board = document.createElement("table");
+        for(let i =0; i < board.length; i++)
+        {
+
+        }
+    }
+}
---
+++ View/loginView.js
@@ -0,0 +1,9 @@
+class View{
+    constructor()
+    {
+        this.login = document.getElementById('login');
+        this.signup = document.getElementById('signup');
+        this.username = document.getElementById('username')
+        this.password = document.getElementById('password');
+    }
+}
