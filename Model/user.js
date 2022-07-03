c41bdd21ed36096f47df5a3d778dc835c16ca945
ohasi <ohadsirkis@gmail.com>
2022-07-03 16:49:40 +0300

    Added more properties to user

--- Model/user.js
+++ Model/user.js
@@ -2,6 +2,10 @@ class User{
     constructor(name, password){
         this.name = name;
         this.password = Symbol(password);
+        this.playerLevel =  1;
+        this.moveCount = 0;
+        this.boardSize = 3;
+        this.joinDate = Date.now;
     }
 
     checkPassword(password){
