1009ffa76ee39e5b624edb00bfcab34fb8f0d982
ohasi <ohadsirkis@gmail.com>
2022-07-04 16:40:23 +0300

    Added leaderboard display to login page

--- View/loginView.js
+++ View/loginView.js
@@ -3,6 +3,7 @@ class LoginView{
     {
         this.startGameButton = document.getElementById('startGame');
         this.usernameInput = document.getElementById('username');
+        this.leaderboard = document.getElementById('highScores');
     }
 
     get username() 
@@ -18,5 +19,17 @@ class LoginView{
     moveToGameView()
     {
         window.location.replace('game.html');
+    }
+
+    displayLeaderboard(result)
+    {
+        let list = document.createElement("ol");
+        highScores.forEach(result => {
+            let item = document.createElement("li");
+            item.innerHTML = `${result.user.name}: ${result.size}*${result.size} in ${result.moves} moves.`
+            list.appendChild(item);
+        });
+        this.leaderboard.firstChild.remove();
+        this.leaderboard.appendChild(list);
     }
 }
