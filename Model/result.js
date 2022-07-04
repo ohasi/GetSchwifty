ca333d48383cdf3f2fb048c6e74e248d9f2ae860
ohasi <ohadsirkis@gmail.com>
2022-07-04 16:19:13 +0300

    Added properties to result

--- Model/result.js
+++ Model/result.js
@@ -1,7 +1,10 @@
 class Result
 {
-    constructor()
+    constructor(user, size, moves, score)
     {
-
+        this.user = user;
+        this.size = size;
+        this.moves = moves;
+        this.score = score;
     }
 }
