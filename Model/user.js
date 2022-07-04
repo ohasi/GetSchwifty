1eae55c06ae17e7557c4e306b16b1bfe833a505c
ohasi <ohadsirkis@gmail.com>
2022-07-04 16:14:05 +0300

    Added result class to save results

--- Model/user.js
+++ Model/user.js
@@ -1,10 +1,13 @@
-class User{
+const MAX_SCORE
+
+class User
+{
     constructor(name)
     {
         this.name = name;
         this.playerLevel =  1;
         this.moveCount = 0;
-        this.boardSize = 3;
+        this.boardSize = 0;
         this.joinDate = Date.now;
     }
 }
